766ce3586b055685fc3139311ee78fe276bb34c6
Ava Chen <avachen@AvadeMacBook-Pro.local>
2014-09-15 13:42:44 +0800

    Initial Commit

---
+++ ReferenceDoctor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ReferenceDoctor
+//
+//  Created by Ava Chen on 9/15/14.
+//  Copyright (c) 2014 chens. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ReferenceDoctor/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  ReferenceDoctor
+//
+//  Created by Ava Chen on 9/15/14.
+//  Copyright (c) 2014 chens. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+            
+@property (weak) IBOutlet NSWindow *window;
+
+
+@end
+
+@implementation AppDelegate
+            
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
